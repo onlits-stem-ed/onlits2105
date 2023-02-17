353724889e4a3386e0298f2e5f7d12c8195f4971
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-02-17 20:01:16 +0530

    20230217

---
+++ Java/source/local/onlits/onlits2105/classobject/student/Address.java
@@ -0,0 +1,8 @@
+package local.onlits.onlits2105.classobject.student;
+
+public class Address {
+    private String street;
+    private String city;
+    private String state;
+    private String pin;
+}
---
+++ Java/source/local/onlits/onlits2105/classobject/student/Student.java
@@ -0,0 +1,13 @@
+package local.onlits.onlits2105.classobject.student;
+
+import java.util.Date;
+
+public class Student {
+    int admissionNumber;
+    String name;
+    char gender;
+    Date dob;
+    String fathersName;
+    String mothersName;
+    Address address;
+}
