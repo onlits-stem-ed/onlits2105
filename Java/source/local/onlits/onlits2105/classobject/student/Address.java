89d0f7fd6cc55ab134aa1f0378e0ec8b353c3cc7
Manish Kumar Mishra <manishmishra.shg@gmail.com>
2023-02-19 20:00:09 +0530

    20230219

--- Java/source/local/onlits/onlits2105/classobject/student/Address.java
+++ Java/source/local/onlits/onlits2105/classobject/student/Address.java
@@ -5,4 +5,37 @@ public class Address {
     private String city;
     private String state;
     private String pin;
+
+    void setStreet(String street) {         //setter method
+        this.street = street;
+    }
+
+    String getStreet() {
+        return street;
+    }
+
+    void setCity(String city) {
+        this.city = city;
+    }
+
+    String getCity() {
+        return city;
+    }
+
+    void setState(String state) {
+        this.state = state;
+    }
+
+    String getState() {
+        return state;
+    }
+
+    void setPin(String pin) {
+        if(pin.length() == 6)
+            this.pin = pin;
+    }
+
+    String getPin() {
+        return pin;
+    }
 }
--- Java/source/local/onlits/onlits2105/classobject/student/Student.java
+++ Java/source/local/onlits/onlits2105/classobject/student/Student.java
@@ -3,11 +3,25 @@ package local.onlits.onlits2105.classobject.student;
 import java.util.Date;
 
 public class Student {
-    int admissionNumber;
-    String name;
-    char gender;
-    Date dob;
-    String fathersName;
-    String mothersName;
-    Address address;
+    private int admissionNumber;
+    private String name;
+    private char gender;
+    private Date dob;
+    private String fathersName;
+    private String mothersName;
+    private Address address = new Address();                //Composition
+
+    void setAdmissionNumber(int admissionNumber) {
+        this.admissionNumber = admissionNumber;
+    }
+
+    int getAdmissionNumber() {
+        return admissionNumber;
+    }
+
+    public static void main(String[] args) {
+        Student s = new Student();
+        s.address.setStreet("Srinagar Colony");
+        System.out.println(s.address.getStreet());
+    }
 }
